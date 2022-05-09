5b2bacd4883519055fcc9ee5c28e91b8d8808dc7
Jeff Stoner <jeff.e.stoner@gmail.com>
2022-05-09 21:10:32 +0000

    more playing around

--- variable_basics/main.go
+++ variable_basics/main.go
@@ -11,4 +11,24 @@ func main() {
 	name, height := "Jeff", 73
 
 	fmt.Printf("My name is %s and I am %d inches tall\n", name, height)
+
+	var (
+		upperLeftX int
+		upperLeftY int
+		lowerRightX int
+		lowerRightY int
+	)
+
+	fmt.Println("Bounding box coordinates: ", upperLeftX, upperLeftY, lowerRightX, lowerRightY)
+
+	var index, counter int
+	index, counter = 0, 1
+
+	// don't do this
+	_, _ = index, counter
+
+	// Silent type casting
+	sum := 5 + 2.3
+	fmt.Println(sum)
+
 }
