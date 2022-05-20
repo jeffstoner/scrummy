fbccdb8939f7e6cbb0020186b45c370603de1ceb
Stoner, Jeff <jeff.stoner@sciencelogic.com>
2022-05-20 12:30:17 -0400

    A few more variable examples

--- variable_basics/main.go
+++ variable_basics/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var age int = 48
@@ -13,8 +16,8 @@ func main() {
 	fmt.Printf("My name is %s and I am %d inches tall\n", name, height)
 
 	var (
-		upperLeftX int
-		upperLeftY int
+		upperLeftX  int
+		upperLeftY  int
 		lowerRightX int
 		lowerRightY int
 	)
@@ -31,4 +34,23 @@ func main() {
 	sum := 5 + 2.3
 	fmt.Println(sum)
 
+	// Go does NOT error on overflow EXCEPT during compile
+	var over1 uint8 = 255 // uint8 max value is 255
+	over1++               // this is fine. it "wraps" around to  0
+	fmt.Println(over1)
+
+	// This is a compiler error
+	// var over2 uint8 = 255 + 1
+
+	// converting numbers
+	var i = 3
+	var f = 3.2
+	var s1, s2 = "3.14", "5"
+
+	var a string = strconv.Itoa(i)
+	var b, _ = strconv.Atoi(s2)
+	var c string = fmt.Sprintf("%.2f", f)
+	var d, _ = strconv.ParseFloat(s1, 64)
+
+	fmt.Println(a, b, c, d)
 }
